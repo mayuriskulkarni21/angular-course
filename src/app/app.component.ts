91c66659b01b06cb91724d8a2a819bcbd810cba6
Mayuri S Kulkarni <mayu.s.kulkarni21@gmail.com>
2025-01-02 16:29:40 +0530

    @Input and @Output concepts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CourseCardComponent } from './course-card/course-card.component';
+import { COURSES } from './db-data';
+import { Course } from './model/course';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CourseCardComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular-course';
+  data = {
+    title: 'angular-course',
+  };
+  angularCourse = COURSES[0];
+  RxJsCourse = COURSES[1];
+  NgRxCourse = COURSES[2];
+
+  onClick() {
+    alert('Hello!!');
+  }
+
+  onKeyUp(newTitle: string) {
+    this.data.title = newTitle;
+  }
+
+  onCardView(course: Course) {
+    console.log('into app component', course);
+  }
 }
---
+++ src/app/course-card/course-card.component.ts
@@ -0,0 +1,27 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Course } from '../model/course';
+
+@Component({
+  selector: 'app-course-card',
+  imports: [],
+  templateUrl: './course-card.component.html',
+  styleUrl: './course-card.component.css',
+})
+export class CourseCardComponent {
+  @Input()
+  course: any;
+
+  @Output()
+  eventEmmitter = new EventEmitter<Course>();
+
+  constructor() {
+    this.course = {};
+  }
+
+  ngOnInit() {}
+
+  onViewDetails() {
+    console.log('into course card component');
+    this.eventEmmitter.emit(this.course);
+  }
+}
